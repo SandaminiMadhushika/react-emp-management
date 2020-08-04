c07823bacb7d6a82ee7b2a3c20aa9d36d1fb1ecf
Sandamini madhushika <sandaminimadhushika96@gmail.com>
2020-08-04 10:19:54 +0530

    react-app-second-task

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -13,3 +13,25 @@ A
 public/index.html,f/4/f43ecdf8d666ee908aacce037b4cd72159e4741b
 ;
 src/App.tsx,8/3/835da56f32df714aaed6285a5b7fc3a7807851e4
+;
+src/App.css,a/a/aa7165aefcf8ee3d73539e99ab007daf1aa18d8d
+<
+package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
+=
+tsconfig.json,6/1/61ebb9fd6e8cf9082658121d5d81e297791dacd0
+A
+public/robots.txt,d/a/dac799070c2d0c2fa24462b02c1f2dcc3fb3a1e9
+A
+package-lock.json,f/a/fa288d1472d29beccb489a676f68739ad365fc47
+9
+	yarn.lock,5/3/53585a096fe3a3b96321113856ae62602995a33b
+@
+src/App.test.tsx,1/f/1fc1c06a0502bf303f8ba35e530938a98e7e35b2
+I
+src/component/welcome.tsx,e/3/e3687f7f3ac15c9d8040066d01b87899823502c3
+P
+ src/component/models/addUser.tsx,e/1/e1809b94d592f92b270972cbcc70017b95791774
+S
+#src/component/models/deleteUser.tsx,b/0/b0059285c194fba07e2ce24e7e5ccb27d95f2ac7
+Q
+!src/component/models/editUser.tsx,8/9/890edb2cd35df591192c9be6b84ad77d880151d0
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 // import logo from './logo.svg';
 import './App.css';
+import {addUser} from './component/models/addUser';
+import {welcome} from './component/welcome';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Weclome to HMS Emp managment Portal
-        </p>
+      <div>
+      <div className="App">
+          {/*<header className="App-header">*/}
+          {/*</header>*/}
 
-      </header>
-    </div>
-  );
+          <Router>
+              <div>
+
+                  <Switch>
+                  <Route exact path='/' component={welcome} />
+              <Route exact path='/adduser' component={addUser} />
+                  </Switch>
+              </div>
+          </Router>
+      </div>
+      </div>
+);
+}
+
+
+function About() {
+    return <h2>About</h2>;
+}
+function Home() {
+    return <h2>Homemy</h2>;
 }
 
 export default App;
